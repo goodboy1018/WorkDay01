c5c11b514310b1b5d5fff393bae6d7df88196b41
neng <1298244473@qq.com>
2022-11-22 10:01:35 +0800

    git for

--- src/main/java/com/company/org/test/TestDemo.java
+++ src/main/java/com/company/org/test/TestDemo.java
@@ -14,14 +14,7 @@ public class TestDemo {
         System.out.println("hello github");
         System.out.println(" push  test");
 
-        System.out.println("commit test");
-        System.out.println("hello github");
-        System.out.println("hello git4");
 
-        System.out.println("hello github");
-        System.out.println(" push  test");
-
-        System.out.println("commit test");
 
     }
 }
